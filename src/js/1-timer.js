0fc38aee02d2db134c5fb5078d3be87d1794a898
Volodymyr Naumchyk <162045667+NaumchykV@users.noreply.github.com>
2024-07-22 08:37:26 +0300

    HW10 timer.js update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -19,7 +19,12 @@ const options = {
   onClose(selectedDates) {
     const selectedDate = selectedDates[0];
     if (selectedDate <= new Date()) {
-      alert('Please choose a date in the future');
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+        messageColor: 'white',
+        backgroundColor: 'red',
+      });
       startButton.disabled = true;
     } else {
       userSelectedDate = selectedDate;
@@ -31,14 +36,11 @@ const options = {
 flatpickr(datetimePicker, options);
 
 startButton.addEventListener('click', () => {
-  if (!userSelectedDate) {
-    window.alert('Please choose a valid date');
-    return;
-  }
+  datetimePicker.disabled = true;
+  startButton.disabled = true;
   const countdown = setInterval(() => {
     const now = new Date();
     const timeDifference = userSelectedDate - now;
-    startButton.disabled = true;
 
     if (timeDifference <= 0) {
       clearInterval(countdown);
@@ -55,19 +57,14 @@ startButton.addEventListener('click', () => {
 });
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
